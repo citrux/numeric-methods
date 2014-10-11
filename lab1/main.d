cec3508e9e29c0c6eee3ad58accf1c0f54b1a047
Vova Abdrakhmanov <369565@gmail.com>
2014-10-11 21:32:38 +0400

    добавлен подсчёт времени работы

--- lab1/main.d
+++ lab1/main.d
@@ -4,14 +4,97 @@ import integral;
 
 import std.stdio;
 import std.math;
+import std.datetime;
 
 void test(real function(real) f, real left, real right, size_t n)
 {
-    writeln("lagrange       : ", lagrange!f(left, right, n));
-    writeln("polynomial     : ", polynomial!f(left, right, n));
-    writeln("gaussLejendre  : ", gaussLejendre!f(left, right, n));
-    writeln("gaussChebyshev : ", gaussChebyshev!f(left, right, n));
-    writeln("tanh-sinh      : ", tanhSinh!f(left, right, n));
+    SysTime ts;
+    real t, s;
+
+    ts = Clock.currTime();
+    s = lagrange!f(left, right, n);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("lagrange       : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = polynomial!f(left, right, n);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("polynomial     : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = gaussLejendre!f(left, right, n);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("gaussLejendre  : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = gaussChebyshev!f(left, right, n);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("gaussChebyshev : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = tanhSinh!f(left, right, n);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("tanhSinh       : %4.3f, %4.3f ms", s, t);
+
+    writeln();
+}
+
+void testPrec(real function(real) f, real left, real right, real precision)
+{
+    SysTime ts;
+    real t, s;
+
+    ts = Clock.currTime();
+    s = integrate!(leftRectangles, f)(left, right, precision);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("leftRectangles   : %4.3f , %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = integrate!(rightRectangles, f)(left, right, precision);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("rightRectangles  : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = integrate!(middleRectangles, f)(left, right, precision);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("middleRectangles : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = integrate!(trapezoids, f)(left, right, precision);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("trapezoids       : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = integrate!(parabolas, f)(left, right, precision);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("parabolas        : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = integrate!(polynomial, f)(left, right, precision);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("polynomial       : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = integrate!(lagrange, f)(left, right, precision);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("lagrange         : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = integrate!(gaussLejendre, f)(left, right, precision);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("gaussLejendre    : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = integrate!(gaussChebyshev, f)(left, right, precision);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("gaussChebyshev   : %4.3f, %4.3f ms", s, t);
+
+    ts = Clock.currTime();
+    s = integrate!(tanhSinh, f)(left, right, precision);
+    t = (Clock.currTime().stdTime() - ts.stdTime()) / 10000.0L;
+    writefln("tanhSinh         : %4.3f, %4.3f ms", s, t);
+    writeln();
+
     writeln();
 }
 
@@ -67,143 +150,53 @@ void demo()
 }
 
 
-void demoAdaptive()
+void demoPrecision()
 {
     real left = 0, right = 4, precision = 1e-3;
 
-    writeln("Адаптивные методы с точностью ", precision);
+    writeln("Интегрирование с точностью ", precision);
     auto f = function(real x){return 1 / sqrt(x);};
     writeln("int_0^4 1 / sqrt(x) dx");
-    writeln("leftRectangles   : ", integrate!(leftRectangles, f)(left, right, precision));
-    writeln("rightRectangles  : ", integrate!(rightRectangles, f)(left, right, precision));
-    writeln("middleRectangles : ", integrate!(middleRectangles, f)(left, right, precision));
-    writeln("trapezoids       : ", integrate!(trapezoids, f)(left, right, precision));
-    writeln("parabolas        : ", integrate!(parabolas, f)(left, right, precision));
-    writeln("polynomial       : ", integrate!(polynomial, f)(left, right, precision));
-    writeln("lagrange         : ", integrate!(lagrange, f)(left, right, precision));
-    writeln("gaussLejendre    : ", integrate!(gaussLejendre, f)(left, right, precision));
-    writeln("gaussChebyshev   : ", integrate!(gaussChebyshev, f)(left, right, precision));
-    writeln("tanhSinh         : ", integrate!(tanhSinh, f)(left, right, precision));
-    writeln();
+    testPrec(f, left, right, precision);
 
     f = function(real x){return 1 / sqrt(x * (4 - x));};
     writeln("int_0^4 1 / sqrt(x(4-x)) dx: ");
-    writeln("leftRectangles   : ", integrate!(leftRectangles, f)(left, right, precision));
-    writeln("rightRectangles  : ", integrate!(rightRectangles, f)(left, right, precision));
-    writeln("middleRectangles : ", integrate!(middleRectangles, f)(left, right, precision));
-    writeln("trapezoids       : ", integrate!(trapezoids, f)(left, right, precision));
-    writeln("parabolas        : ", integrate!(parabolas, f)(left, right, precision));
-    writeln("polynomial       : ", integrate!(polynomial, f)(left, right, precision));
-    writeln("lagrange         : ", integrate!(lagrange, f)(left, right, precision));
-    writeln("gaussLejendre    : ", integrate!(gaussLejendre, f)(left, right, precision));
-    writeln("gaussChebyshev   : ", integrate!(gaussChebyshev, f)(left, right, precision));
-    writeln("tanhSinh         : ", integrate!(tanhSinh, f)(left, right, precision));
-    writeln();
+    testPrec(f, left, right, precision);
 
     f = function(real x){return sqrt(x * (4 - x));};
     writeln("int_0^4 sqrt(x(4-x)) dx: ");
-    writeln("leftRectangles   : ", integrate!(leftRectangles, f)(left, right, precision));
-    writeln("rightRectangles  : ", integrate!(rightRectangles, f)(left, right, precision));
-    writeln("middleRectangles : ", integrate!(middleRectangles, f)(left, right, precision));
-    writeln("trapezoids       : ", integrate!(trapezoids, f)(left, right, precision));
-    writeln("parabolas        : ", integrate!(parabolas, f)(left, right, precision));
-    writeln("polynomial       : ", integrate!(polynomial, f)(left, right, precision));
-    writeln("lagrange         : ", integrate!(lagrange, f)(left, right, precision));
-    writeln("gaussLejendre    : ", integrate!(gaussLejendre, f)(left, right, precision));
-    writeln("gaussChebyshev   : ", integrate!(gaussChebyshev, f)(left, right, precision));
-    writeln("tanhSinh         : ", integrate!(tanhSinh, f)(left, right, precision));
-    writeln();
+    testPrec(f, left, right, precision);
 
     f = function(real x){return sqrt(x);};
     writeln("int_0^4 sqrt(x) dx: ");
-    writeln("leftRectangles   : ", integrate!(leftRectangles, f)(left, right, precision));
-    writeln("rightRectangles  : ", integrate!(rightRectangles, f)(left, right, precision));
-    writeln("middleRectangles : ", integrate!(middleRectangles, f)(left, right, precision));
-    writeln("trapezoids       : ", integrate!(trapezoids, f)(left, right, precision));
-    writeln("parabolas        : ", integrate!(parabolas, f)(left, right, precision));
-    writeln("polynomial       : ", integrate!(polynomial, f)(left, right, precision));
-    writeln("lagrange         : ", integrate!(lagrange, f)(left, right, precision));
-    writeln("gaussLejendre    : ", integrate!(gaussLejendre, f)(left, right, precision));
-    writeln("gaussChebyshev   : ", integrate!(gaussChebyshev, f)(left, right, precision));
-    writeln("tanhSinh         : ", integrate!(tanhSinh, f)(left, right, precision));
-    writeln();
+    testPrec(f, left, right, precision);
 
     f = function(real x){return x;};
     writeln("int_0^4 x dx: ");
-    writeln("leftRectangles   : ", integrate!(leftRectangles, f)(left, right, precision));
-    writeln("rightRectangles  : ", integrate!(rightRectangles, f)(left, right, precision));
-    writeln("middleRectangles : ", integrate!(middleRectangles, f)(left, right, precision));
-    writeln("trapezoids       : ", integrate!(trapezoids, f)(left, right, precision));
-    writeln("parabolas        : ", integrate!(parabolas, f)(left, right, precision));
-    writeln("polynomial       : ", integrate!(polynomial, f)(left, right, precision));
-    writeln("lagrange         : ", integrate!(lagrange, f)(left, right, precision));
-    writeln("gaussLejendre    : ", integrate!(gaussLejendre, f)(left, right, precision));
-    writeln("gaussChebyshev   : ", integrate!(gaussChebyshev, f)(left, right, precision));
-    writeln("tanhSinh         : ", integrate!(tanhSinh, f)(left, right, precision));
-    writeln();
+    testPrec(f, left, right, precision);
 
     f = function(real x){return 1;};
     writeln("int_0^4 dx: ");
-    writeln("leftRectangles   : ", integrate!(leftRectangles, f)(left, right, precision));
-    writeln("rightRectangles  : ", integrate!(rightRectangles, f)(left, right, precision));
-    writeln("middleRectangles : ", integrate!(middleRectangles, f)(left, right, precision));
-    writeln("trapezoids       : ", integrate!(trapezoids, f)(left, right, precision));
-    writeln("parabolas        : ", integrate!(parabolas, f)(left, right, precision));
-    writeln("polynomial       : ", integrate!(polynomial, f)(left, right, precision));
-    writeln("lagrange         : ", integrate!(lagrange, f)(left, right, precision));
-    writeln("gaussLejendre    : ", integrate!(gaussLejendre, f)(left, right, precision));
-    writeln("gaussChebyshev   : ", integrate!(gaussChebyshev, f)(left, right, precision));
-    writeln("tanhSinh         : ", integrate!(tanhSinh, f)(left, right, precision));
-    writeln();
+    testPrec(f, left, right, precision);
 
     f = function(real x){return 1 + x + x * x + x * x * x;};
     writeln("int_0^4 x^3 + x^2 + x + 1 dx: ");
-    writeln("leftRectangles   : ", integrate!(leftRectangles, f)(left, right, precision));
-    writeln("rightRectangles  : ", integrate!(rightRectangles, f)(left, right, precision));
-    writeln("middleRectangles : ", integrate!(middleRectangles, f)(left, right, precision));
-    writeln("trapezoids       : ", integrate!(trapezoids, f)(left, right, precision));
-    writeln("parabolas        : ", integrate!(parabolas, f)(left, right, precision));
-    writeln("polynomial       : ", integrate!(polynomial, f)(left, right, precision));
-    writeln("lagrange         : ", integrate!(lagrange, f)(left, right, precision));
-    writeln("gaussLejendre    : ", integrate!(gaussLejendre, f)(left, right, precision));
-    writeln("gaussChebyshev   : ", integrate!(gaussChebyshev, f)(left, right, precision));
-    writeln("tanhSinh         : ", integrate!(tanhSinh, f)(left, right, precision));
-    writeln();
+    testPrec(f, left, right, precision);
 
     f = function(real x){return log(x);};
     right = 1;
     writeln("int_0^1 ln x dx: ");
-    writeln("leftRectangles   : ", integrate!(leftRectangles, f)(left, right, precision));
-    writeln("rightRectangles  : ", integrate!(rightRectangles, f)(left, right, precision));
-    writeln("middleRectangles : ", integrate!(middleRectangles, f)(left, right, precision));
-    writeln("trapezoids       : ", integrate!(trapezoids, f)(left, right, precision));
-    writeln("parabolas        : ", integrate!(parabolas, f)(left, right, precision));
-    writeln("polynomial       : ", integrate!(polynomial, f)(left, right, precision));
-    writeln("lagrange         : ", integrate!(lagrange, f)(left, right, precision));
-    writeln("gaussLejendre    : ", integrate!(gaussLejendre, f)(left, right, precision));
-    writeln("gaussChebyshev   : ", integrate!(gaussChebyshev, f)(left, right, precision));
-    writeln("tanhSinh         : ", integrate!(tanhSinh, f)(left, right, precision));
-    writeln();
+    testPrec(f, left, right, precision);
 
-    f = function(real x){return log(x) / sqrt(x);};
-    writeln("int_0^1 ln x / sqrt(x) dx: ");
-    writeln("leftRectangles   : ", integrate!(leftRectangles, f)(left, right, precision));
-    //writeln("rightRectangles  : ", integrate!(rightRectangles, f)(left, right, precision));
-    //writeln("middleRectangles : ", integrate!(middleRectangles, f)(left, right, precision));
-    writeln("trapezoids       : ", integrate!(trapezoids, f)(left, right, precision));
-    writeln("parabolas        : ", integrate!(parabolas, f)(left, right, precision));
-    writeln("polynomial       : ", integrate!(polynomial, f)(left, right, precision));
-    writeln("lagrange         : ", integrate!(lagrange, f)(left, right, precision));
-    writeln("gaussLejendre    : ", integrate!(gaussLejendre, f)(left, right, precision));
-    writeln("gaussChebyshev   : ", integrate!(gaussChebyshev, f)(left, right, precision));
-    writeln("tanhSinh         : ", integrate!(tanhSinh, f)(left, right, precision));
-    writeln();
+    //f = function(real x){return log(x) / sqrt(x);};
+    //writeln("int_0^1 ln x / sqrt(x) dx: ");
+    //testPrec(f, left, right, precision);
 }
 
 
 void main()
 {
     demo();
-    demoAdaptive();
+    demoPrecision();
 }
 
