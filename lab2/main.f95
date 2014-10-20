e5c7ab79ff24196dd3a5214e14ea96439fbe581d
Vova Abdrakhmanov <369565@gmail.com>
2014-10-20 08:32:54 +0400

    добавлен метод прогноз-коррекция (pceuler)

--- lab2/main.f95
+++ lab2/main.f95
@@ -1,27 +1,69 @@
 program main
-    real, dimension(20) :: x, y
-    x = (/ (0.05 * (I-1), I = 1, 20) /)
-    y = euler(f, x, 1.0)
-    print *, x
-    print *, y
-    print *, exp(-x)
+    real, dimension(600) :: x
+    real, dimension(2) :: start
+    real, dimension(size(x), size(start)) :: y1, y2
+    start = (/ 1.0, 0.0 /)
+    x = (/ (0.05 * (I-1), I = 1, 600) /)
+    y1 = euler(f, x, start)
+    y2 = pceuler(f, x, start)
+    open(unit=1, file="data.out")
+    do i = 1, size(x)
+        write (1, *) x(i), y1(i, 1), y2(i, 1)
+    end do
+    close(1)
 
 contains
 
     function euler (f, x, y0)
         real, dimension(:) :: x
-        real, dimension(size(x)) :: y, euler
-        real :: y0
+        real, dimension(:) :: y0
+        real, dimension(size(x), size(y0)) :: y, euler
         integer :: i
-        y(1) = y0
+
+        interface
+            pure function f(x, y)
+                real, intent(in) :: x
+                real, dimension(:), intent(in) :: y
+                real, dimension(size(y)) ::  f
+            end function f
+        end interface
+
+        y(1,:) = y0
         do i = 1, size(x) - 1
-            y(i+1) = y(i) + f(x(i), y(i)) * (x(i+1) - x(i))
+            y(i+1,:) = y(i,:) + f(x(i), y(i,:)) * (x(i+1) - x(i))
         end do
         euler = y
     end function euler
 
-    real function f(x, y)
-        f = -y
+    function pceuler (f, x, y0)
+        real, dimension(:) :: x
+        real, dimension(:) :: y0
+        real, dimension(size(y0)) :: z
+        real, dimension(size(x), size(y0)) :: y, pceuler
+        integer :: i
+
+        interface
+            pure function f(x, y)
+                real, intent(in) :: x
+                real, dimension(:), intent(in) :: y
+                real, dimension(size(y)) ::  f
+            end function f
+        end interface
+
+        y(1,:) = y0
+        do i = 1, size(x) - 1
+            z = y(i,:) + f(x(i), y(i,:)) * (x(i+1) - x(i))
+            y(i+1,:) = y(i,:) + (f(x(i), y(i,:)) + f(x(i), z)) * (x(i+1) - x(i))/2
+        end do
+        pceuler = y
+    end function pceuler
+
+    pure function f(x, y)
+        real, intent(in) :: x
+        real, dimension(:), intent(in) :: y
+        real, dimension(size(y)) :: f
+        f(1) = y(2)
+        f(2) = -y(1)
     end function f
 
 end program main
