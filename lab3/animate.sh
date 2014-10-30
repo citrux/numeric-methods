da3829a50d6e3be2c483b431ea76159c6ea41f81
Vova Abdrakhmanov <369565@gmail.com>
2014-10-30 10:48:42 +0300

    добавлено решение уравнения диффузии

---
+++ lab3/animate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+convert -delay 10 PAR_*.png -loop 100 parabolic.gif
---
+++ lab3/main.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void parabolic();
+
+int main(int argc, const char *argv[])
+{
+    parabolic();
+    return 0;
+}
+
+double source(double x, double t)
+{
+    return 0;
+}
+
+double initial_condition(double x)
+{
+    return 10 * (x - x * x + 1);
+}
+
+double lbc_a(double t)
+{
+    return 0;
+}
+double lbc_b(double t)
+{
+    return 1;
+}
+double lbc_c(double t)
+{
+    return 0;
+}
+
+double rbc_a(double t)
+{
+    return 0;
+}
+double rbc_b(double t)
+{
+    return 1;
+}
+double rbc_c(double t)
+{
+    return 10;
+}
+
+void parabolic()
+{
+    double l = 1,
+           tmax = 0.3;
+
+    unsigned int n = 1000,
+                 m = 1000,
+                 k = m / 20,
+                 i, j;
+
+    double h = l / n,
+           tau = tmax / m,
+           r = tau / h / h;
+
+    double *state, *L, *U, *D;
+
+    state = (double*) calloc(sizeof(double), n + 1);
+
+    L = (double*) calloc(sizeof(double), n + 1);
+    U = (double*) calloc(sizeof(double), n + 1);
+    D = (double*) calloc(sizeof(double), n + 1);
+
+    for (i = 0; i <= n; i++)
+        state[i] = initial_condition(i * h);
+
+    for (j = 0; j <= m; j++)
+    {
+        // формирование матрицы
+        for (i = 0; i <= n; i++)
+        {
+            L[i] = -r;
+            U[i] = -r;
+            D[i] = 1 + 2 * r;
+        }
+        L[n-1] = -rbc_a(j * tau) / h;
+        U[1] = lbc_a(j * tau) / h;
+        D[0] = lbc_b(j * tau) - lbc_a(j * tau) / h;
+        D[n] = rbc_b(j * tau) + rbc_a(j * tau) / h;
+
+        // формирование правой части
+        state[0] = lbc_c(j * tau);
+        state[n] = rbc_c(j * tau);
+
+        // прогонка:
+        // прямая
+        for (i = 0; i < n; i++)
+        {
+            D[i+1] -= U[i+1] / D[i] * L[i];
+            state[i+1] -= state[i] / D[i] * L[i];
+        }
+        // обратная
+        state[n] /= D[n];
+        for (i = n; i > 0; i--)
+        {
+            state[i-1] -= state[i] * U[i];
+            state[i-1] /= D[i-1];
+        }
+
+        // нужно вывести, отдать gnuplot и построить графики
+        if (j % k == 0)
+        {
+            FILE* tmp = fopen("data.tmp", "w");
+            for (i = 0; i<=n; i++)
+                fprintf(tmp, "%lf %lf\n", i * h, state[i]);
+            fclose(tmp);
+            FILE* gnuplot = popen("gnuplot", "w");
+            fprintf(gnuplot, "set term pngcairo\n");
+            fprintf(gnuplot, "set yrange [0:12]\n");
+            fprintf(gnuplot, "set output \"PAR_%04d.png\"\n", j / k);
+            fprintf(gnuplot, "plot \"data.tmp\" using 1:2 with lines\n");
+            fclose(gnuplot);
+        }
+    }
+    free(L);
+    free(U);
+    free(D);
+    free(state);
+}
