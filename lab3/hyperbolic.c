6d47f6605a7fd7ce9b93b051e59efbc1c60bcb6d
Vova Abdrakhmanov <369565@gmail.com>
2014-11-14 11:15:04 +0300

    Полностью переписал решение гиперболического уравнения, но проблемы остались

--- lab3/hyperbolic.c
+++ lab3/hyperbolic.c
@@ -2,187 +2,173 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define PI 3.14159
+void solveThomas(int n,
+           const double const *l,
+           const double const *d,
+           const double const *u,
+           double *r)
+{
+    int i;
+    double d1[n];
 
-double source(double x, double t)
+    d1[0] = d[0];
+    for (i = 0; i < n - 1; i++)
+    {
+        d1[i+1] = d[i+1] - u[i+1] * l[i] / d1[i];
+        r[i+1] -= r[i] * l[i] / d1[i];
+    }
+
+    r[n-1] /= d1[n-1];
+    for (i = n-1; i > 0; i--)
+    {
+        r[i-1] -= r[i] * u[i];
+        r[i-1] /= d1[i-1];
+    }
+}
+
+void swap3(double **a, double **b, double **c)
+{
+    double *tmp;
+    tmp = *c;
+    *c = *b;
+    *b = *a;
+    *a = tmp;
+}
+
+void initPlot(char *name)
+{
+    char fname[20];
+    sprintf(fname, "%s.gp", name);
+    FILE *file = fopen(fname, "w");
+    fprintf(file, "set term gif ");
+    fprintf(file, "animate ");
+    fprintf(file, "delay 1 ");
+    fprintf(file, "loop 1 ");
+    fprintf(file, "size 600, 600 ");
+    fprintf(file, "background \"#ffffff\" \n");
+    fprintf(file, "set output '%s.gif' \n", name);
+    fprintf(file, "unset key\n");
+    fprintf(file, "set xrange[0:1]\n");
+    fprintf(file, "set yrange[-1.2:1.2]\n");
+    fprintf(file, "set grid x y\n");
+    fclose(file);
+}
+
+void addToPlot(char *name, double *x, double *y, int n)
+{
+    char fname[20];
+    sprintf(fname, "%s.gp", name);
+    FILE *file = fopen(fname, "a");
+    int i;
+
+    fprintf(file, "plot \"-\" w l lw 2 lc \"#222222\"\n");
+    for (i = 0; i < n; i++)
+        fprintf(file, "%f %f\n", x[i], y[i]);
+    fprintf(file, "end\n");
+
+    fclose(file);
+}
+
+// Граничные условия
+double a0(double t)
 {
     return 0;
 }
 
-double initial_condition(double x)
-{
-    return (x > 0.333 && x < 0.667) ? 12 * (0.166-fabs(x - 0.5)) : 0;
-}
-
-double initial_condition2(double x)
-{
-    return 0;
-}
-
-double lbc_a(double t)
-{
-    return 0;
-}
-
-double lbc_b(double t)
+double b0(double t)
 {
     return 1;
 }
 
-double lbc_c(double t)
+double c0(double t)
 {
     return 0;
 }
 
-double rbc_a(double t)
+double a1(double t)
 {
     return 0;
 }
 
-double rbc_b(double t)
+double b1(double t)
 {
     return 1;
 }
 
-double rbc_c(double t)
+double c1(double t)
+{
+    return 0;
+}
+
+// Начальные условия
+double u0(double x)
+{
+    return sin(M_PI * x);
+}
+
+double du0(double x)
 {
     return 0;
 }
 
 int main(int argc, const char *argv[])
 {
-    if (argc != 4)
-    {
-        puts("Неправильное число аргументов!");
-        printf("Пример использования: %s 1 5.0 wave1\n", argv[0]);
-        puts("Подробнее в readme.md");
-        return 1;
-    }
-
-    int implicit = atoi(argv[1]);
-    double tmax = atof(argv[2]);
-    const char* outfile = argv[3];
-
-    double l = 1;
-
-    unsigned int n = 100, m, i, j, k;
-
-    double h = l / n, tau, r, fps = 10;
-
-    if (implicit)
-    {
-        k = 2;
-        tau = 1.0 / k / fps;
-        r = tau * tau / h / h;
-    }
+    char* fname;
+    if (argc == 2)
+        fname = argv[1];
     else
-    {
-        r = 0.9025;
-        tau = 0.95 * h;
-        k = (int) (1.0 / tau / fps);
-    }
+        fname = "noname";
 
-    m = (int)(tmax / tau);
+    double l = 1,
+           tmax = 20;
+    int n = 100, m = 200, i, j, t;
+    double dx = l / n, dt = tmax / m, r = dt * dt / dx / dx;
+    double *x, *u, *up, *un, *ml, *md, *mu;
 
-    double *state, *prev, *next, *tmp, *L, *U, *D;
+    x = calloc(sizeof(double), n + 1);
+    u = calloc(sizeof(double), n + 1);
+    up = calloc(sizeof(double), n + 1);
+    un = calloc(sizeof(double), n + 1);
+    ml = calloc(sizeof(double), n + 1);
+    md = calloc(sizeof(double), n + 1);
+    mu = calloc(sizeof(double), n + 1);
 
-    state = (double*) calloc(sizeof(double), n + 1);
-    prev = (double*) calloc(sizeof(double), n + 1);
-    next = (double*) calloc(sizeof(double), n + 1);
-
-    if (implicit)
-    {
-        L = (double*) calloc(sizeof(double), n + 1);
-        U = (double*) calloc(sizeof(double), n + 1);
-        D = (double*) calloc(sizeof(double), n + 1);
-    }
+    t = 0;
+    initPlot(fname);
 
     for (i = 0; i <= n; i++)
     {
-        prev[i] = initial_condition(i * h);
-        state[i] = prev[i] + initial_condition2(i * h) * tau;
+        x[i] = i * dx;
+        u[i] = u0(i * dx);
+        up[i] = u[i] - du0(i * dx) * dt;
     }
 
-    FILE* gnuplot = popen("gnuplot", "w");
-    fprintf(gnuplot, "set term pngcairo\n");
-    fprintf(gnuplot, "set yrange [-5: 5]\n");
-
-    for (j = 0; j <= m; j++)
+    for (j = 0; j < m; j++)
     {
-        if (implicit)
+        for (i = 0; i <= n; i++)
         {
-            // формирование матрицы
-            for (i = 0; i <= n; i++)
-            {
-                L[i] = -r / 2;
-                U[i] = -r / 2;
-                D[i] = 1 + r;
-            }
-            L[n-1] = -rbc_a(j * tau) / h;
-            U[1] = lbc_a(j * tau) / h;
-            D[0] = lbc_b(j * tau) - lbc_a(j * tau) / h;
-            D[n] = rbc_b(j * tau) + rbc_a(j * tau) / h;
+            md[i] = 1 + r / 2;
+            mu[i] = ml[i] = -r / 4;
 
-            // формирование правой части
-            next[0] = lbc_c(j * tau);
-
-            for (i = 1; i < n; i++)
-                next[i] = -(1 + r) * prev[i] + 2 * state[i] +
-                    r / 2 * (prev[i - 1] + prev[i + 1]) +
-                    tau * tau * source(i*h, j*tau);
-
-            next[n] = rbc_c(j * tau);
-
-            // прогонка:
-            // прямая
-            for (i = 0; i < n; i++)
-            {
-                D[i+1] -= U[i+1] / D[i] * L[i];
-                next[i+1] -= next[i] / D[i] * L[i];
-            }
-            // обратная
-            next[n] /= D[n];
-            for (i = n; i > 0; i--)
-            {
-                next[i-1] -= next[i] * U[i];
-                next[i-1] /= D[i-1];
-            }
+            un[i] = 2 * u[i] - up[i] +
+                    r/4 * (up[i-1] - 2 * up[i] + up[i+1]) +
+                    r/2 * (u[i-1]  - 2 * u[i]  + u[i+1]);
         }
-        else
-        {
-            // считаем середину
-            for (i = 1; i < n; i++) {
-                next[i] = 2 * (1 - r) * state[i] + r * (state[i-1] + state[i+1])
-                      - prev[i] + tau * tau * source(i * h, j * tau);
-            }
-            // считаем края
-            next[0] = (lbc_c(j * tau) - state[1] * lbc_a(j * tau) / h) /
-                      (lbc_b(j * tau) - lbc_a(j * tau) / h);
-            next[n] = (rbc_c(j * tau) + state[n-1] * rbc_a(j * tau) / h) /
-                      (rbc_b(j * tau) + rbc_a(j * tau) / h);
-        }
+        md[0] = b0(t) - a0(t) / dx;
+        mu[1] = a0(t) / dx;
+        un[0] = c0(t);
 
-        tmp = prev;
-        prev = state;
-        state = next;
-        next = tmp;
+        md[n] = b1(t) + a1(t) / dx;
+        ml[n-1] = -a1(t) / dx;
+        un[n] = c1(t);
 
-        // нужно вывести, отдать gnuplot и построить графики
-        if (j % k == 0)
-        {
-            char fname[20];
-            sprintf(fname, "temp/%s_%04d.tmp", outfile, j / k);
-            FILE* tmp = fopen(fname, "w");
-            for (i = 0; i<=n; i++)
-                fprintf(tmp, "%lf %lf\n", i * h, state[i]);
-            fclose(tmp);
-            fprintf(gnuplot, "set output \"temp/%s_%04d.png\"\n", outfile, j / k);
-            fprintf(gnuplot, "plot \"%s\" with lines title \"%f\"\n", fname, j * tau);
-        }
+        solveThomas(n + 1, ml, md, mu, un);
+
+        swap3(&un, &u, &up);
+
+        addToPlot(fname, x, u, n + 1);
+
+        t += dt;
     }
-    fclose(gnuplot);
-    free(prev);
-    free(state);
     return 0;
 }
-
