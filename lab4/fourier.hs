be97d24ee32d935b5e4bed2a6c93477259255d48
Vova Abdrakhmanov <369565@gmail.com>
2014-11-28 18:32:38 +0300

    БПФ на Haskell

---
+++ lab4/fourier.hs
@@ -0,0 +1,18 @@
+module Fourier where
+
+import Data.Complex
+
+-- Cooley-Tukey
+fft :: RealFloat a => [a] -> [Complex a]
+fft [] = []
+fft [x] = [x :+ 0]
+fft xs = zipWith (+) ys ts ++ zipWith (-) ys ts
+    where n = length xs
+        ys = fft evens
+        zs = fft odds
+        (evens, odds) = split xs
+        split [] = ([], [])
+        split [x] = ([x], [])
+        split (x:y:xs) = (x:xt, y:yt) where (xt, yt) = split xs
+        ts = zipWith (\z k -> exp' k n * z) zs [0 .. ]
+        exp' k n = cis $ 2 * pi * (fromIntegral k) / (fromIntegral n)
--- lab4/wavelet.hs
+++ lab4/wavelet.hs
@@ -1,51 +1,26 @@
+module Wavelet where
+
 import Data.Complex;
-import Text.Printf;
 
-main = printTable [0.1, 0.15 .. 2] [0.01, 0.02 .. 0.99]
-
-printTable [] ys = putStrLn ""
-printTable (x:xs) ys = do
-        printRow x ys
-        printTable xs ys
-
-printRow x [] = putStrLn ""
-printRow x (y:ys) = do
-        printf "%f %f %f\n" x y (s x y)
-        printRow x ys
-
-f :: Double -> Double
-{-f x = exp t where t = -2 * (x - 0.5) ** 2-}
-f x
-    {-| (abs(x-0.5) < 0.1) = sin (2 * pi * x)-}
-    | (abs(x-0.3) < 0.1) = cos (pi * x / 4)
-    | otherwise          = 0
-
-morle :: Double -> Double -> Double -> Complex Double
+morle :: RealFloat a => a -> a -> a -> Complex a
 morle alpha k0 t = (exp x) * ((exp y) - (exp z))
     where
         x = - toComplex ((t / alpha) ** 2)
         y = (0:+1) * toComplex (k0 * t)
         z = - toComplex ((k0 * alpha / 2) ** 2)
 
-psi = morle 0.5 (2 * pi)
-
-transform :: [Double] -> Double -> Double -> Complex Double
-transform ts a b = sum scal_prods / norm
+transform :: RealFloat a => [a] -> (a -> Complex a) -> a -> a -> Complex a
+transform xs wavelet scale time = sum scal_prods / norm
     where
-        scal_prod t = (toComplex . f) t * (conjugate . psi) ((t - b) / a)
-        scal_prods = map scal_prod ts
-        norm = (toComplex . sum) $ map (\t -> exp (-((t - b) / a / 0.5) ** 2)) ts
+        n = length xs
+        scal_prods = zipWith (*) (map toComplex xs) ws
+        ts = map (\t -> fromIntegral t / fromIntegral n) [0 .. n - 1]
+        ws = map (\t -> conjugate $ wavelet $ (t - time) / scale) ts
+        norm = (toComplex . sum) $ map (\t -> exp (-((t - time) / scale / 0.5) ** 2)) ts
 
-toComplex :: Double -> Complex Double
+toComplex :: RealFloat a => a -> Complex a
 toComplex x = x :+ 0
 
-cabs :: Complex Double -> Double
-cabs x = t where (t, a) = polar x
-
-ts :: [Double]
-ts = map (\i -> i / 99) [0 .. 99]
-
-w = transform ts
-
-s :: Double -> Double -> Double
-s a b = cabs(w a b) ** 2
+cabs :: RealFloat a => Complex a -> a
+cabs x = t
+    where (t, _) = polar x
